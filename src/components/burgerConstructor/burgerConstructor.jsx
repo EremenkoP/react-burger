05db39f917a5ff9c6ae143dbba05cc934a79031f
EremenkoP <Eremenko.p.a@yandex.ru>
2022-04-09 20:41:42 +0400

    РЕализовал предворительное
    Сделал все по предварительному,  и чуть чуть второго.

--- src/components/burgerConstructor/burgerConstructor.jsx
+++ src/components/burgerConstructor/burgerConstructor.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 
 import style from "./burgerConstructor.module.css"
 
+const Order = ({data}) => {
+  return (
+    <div className={" mt-10 " + style.price}>
+    <p className="mr-10 text text_type_digits-medium">
+      <CurrencyIcon type="primary" />
+    </p>
+    <Button type="primary" size="large">
+      Оформить заказ
+    </Button>
+  </div>
+  )
+}
+
 const BurgerConstructor = ({ingredients}) => {
+
   return (
     <>
       <div className={" pr-2 "+style.container}>
         <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-          <div  key={ingredients[0]._id} className={style.element}>
+          <div  key={'1'} className={style.element}>
             <ConstructorElement
               type="top"
               isLocked={true}
-              text={ingredients[0].name}
-              price={ingredients[0].price}
-              thumbnail={ingredients[0].image_mobile}
+              text="Краторная булка N-200i (верх)"
+              price={200}
+              thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
           />
            </div>
           {ingredients
@@ -31,27 +46,24 @@ const BurgerConstructor = ({ingredients}) => {
                 </div>
               </>
           ))}
-          <div  key={ingredients[0]._id} className={style.element}>
+          <div  key={"2"} className={style.element}>
             <ConstructorElement
               type="bottom"
               isLocked={true}
-              text={ingredients[0].name}
-              price={ingredients[0].price}
-              thumbnail={ingredients[0].image_mobile}
+              text="Краторная булка N-200i (низ)"
+              price={200}
+              thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
           />
            </div>
         </div>
       </div>
-      <div className={" mt-10 " + style.price}>
-        <p className="mr-10 text text_type_digits-medium">
-          <CurrencyIcon type="primary" />
-        </p>
-        <Button type="primary" size="large">
-          Оформить заказ
-        </Button>
-      </div>
+      <Order data={ingredients}/>
     </>
   )
 }
+
+BurgerConstructor.propTypes = {
+  ingredients: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default BurgerConstructor;
